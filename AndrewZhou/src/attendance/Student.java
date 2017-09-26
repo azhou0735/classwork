6f2711ba175c515e50f825cd5d3e7149c72b7f55
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-09-26 11:46:21 -0400

    finish @ home

---
+++ AndrewZhou/src/attendance/Student.java
@@ -0,0 +1,51 @@
+package attendance;
+
+public class Student implements Attendee {
+	private String firstName;
+	private String lastName;
+	private boolean presentStatus = false;
+	public Student(String first, String last)
+	{
+		String firstName = first;
+		String lastName = last;
+	}
+	public boolean isPresent() {
+		return presentStatus;
+	}
+	public void setPresent(boolean present) {
+		if(present)
+		{
+			presentStatus = true;
+		}
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public boolean mathces(String first, String last) {
+		return first.toLowerCase().equals(last.toLowerCase());
+	}
+	public boolean matches(String last) {
+		return this.getLastName().toLowerCase().equals(last.toLowerCase());
+	}
+	public String getReportString() {
+		String first = this.getFirstName();
+		String last = this.getLastName();
+		boolean status = this.isPresent();
+		String returnString;
+		if(this.getFirstName()>20)
+		{
+			last = this.getLastName().substring(0,17) + "...";
+		}
+		returnString = last;
+		for(int i = returnString.length(); i<20; i++)
+		{
+			returnString += " ";
+		}
+		return returnString;
+		
+	}
+	
+}
