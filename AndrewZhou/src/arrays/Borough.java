e1e025c9c180c2b6534d776f92d2e012ffdbc808
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-10-18 10:39:32 -0400

    10-18

--- AndrewZhou/src/arrays/Borough.java
+++ AndrewZhou/src/arrays/Borough.java
@@ -1,4 +1,4 @@
-package work;
+package arrays;
 
 public class Borough {
 	private String name;
---
+++ AndrewZhou/src/arrays/Person.java
@@ -0,0 +1,15 @@
+package arrays;
+
+import work.Borough;
+
+public class Person {
+	private String firstName;
+	private String lastName;
+	private Borough home;
+			
+	public Person(String f, String l, Borough h) {
+		this.firstName = f;
+		this.lastName = l;
+		this.home = h;
+	}
+}
