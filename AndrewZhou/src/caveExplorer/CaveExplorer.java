2aee23b70ec318776f3569aab0d6981fc103051f
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-10-23 11:41:28 -0400

    10-23 Cave Explorer project started

---
+++ AndrewZhou/src/caveExplorer/CaveExplorer.java
@@ -0,0 +1,9 @@
+package caveExplorer;
+
+public class CaveExplorer {
+
+		public static CaveRoom[][] caves; //every room in the cave
+		public static Scanner in;
+		public static CaveRoom currentRoom;
+		public static Inventory inventory;
+}
