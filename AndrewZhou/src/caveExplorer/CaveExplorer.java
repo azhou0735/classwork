c67b4085e8fcc58a32fae6ae17dae6182cf9c218
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-10-23 11:41:45 -0400

    10-23 Forgot to save

--- AndrewZhou/src/caveExplorer/CaveExplorer.java
+++ AndrewZhou/src/caveExplorer/CaveExplorer.java
@@ -3,7 +3,10 @@ package caveExplorer;
 public class CaveExplorer {
 
 		public static CaveRoom[][] caves; //every room in the cave
-		public static Scanner in;
-		public static CaveRoom currentRoom;
-		public static Inventory inventory;
+		public static Scanner in; //for user input
+		public static CaveRoom currentRoom; //changes based on user navigation
+		public static Inventory inventory; //objects found in cave are stored
+		
+		public static void main(String[] args) {
+		}
 }
