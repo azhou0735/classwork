a105396ed79eadd40c7d383123c1f07af2ccc7df
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-11-01 11:45:14 -0400

    11-1

--- AndrewZhou/src/caveExplorer/CaveExplorer.java
+++ AndrewZhou/src/caveExplorer/CaveExplorer.java
@@ -9,6 +9,7 @@ public class CaveExplorer {
 		public static CaveRoom currentRoom; //changes based on user navigation
 		public static Inventory inventory; //objects found in cave are stored
 		public static boolean playing = true;
+		public static NPC[] npcs;
 		
 		public static void main(String[] args) {
 			in = new Scanner(System.in);
@@ -16,20 +17,26 @@ public class CaveExplorer {
 			
 			inventory = new Inventory();
 			startExploring();
-			
 		}
 
 		private static void startExploring() {
 			while(playing) {
+				npcActions();
 				print(inventory.getDescription());
 				print(currentRoom.getDescription());
 				print("What would you like to do?");
 				String input = in.nextLine();
 				currentRoom.interpretInput(input);
 			}
-			
 		}
 		
+		private static void npcActions() {
+			for(NPC n: npcs) {
+				n.act();
+			}
+			inventory.updateMap();
+		}
+
 		public static void print(String s) {
 			//NOTE: later, you can replace this line with the more sophisticated "multiLinePrint"
 			System.out.println(s);
