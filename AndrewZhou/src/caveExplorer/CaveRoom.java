f7d70dc2b4cab3bdac7ce746f62cd098eaa0b174
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-11-02 11:45:16 -0400

    11-2

--- AndrewZhou/src/caveExplorer/CaveRoom.java
+++ AndrewZhou/src/caveExplorer/CaveRoom.java
@@ -169,35 +169,31 @@ public class CaveRoom {
 	public static void setUpCaves() {
 		//ALL OF THIS CODE CAN BE CHANGED
 		//1. Decide how big your caves should be
-		CaveExplorer.caves = new NPCRoom[5][5];
+			CaveExplorer.caves = new NPCRoom[5][7];
+			
 		//2. Populate with caves and a defualt description: hint: when starting, use coordinates (helps debugging)
-		for(int row = 0; row < CaveExplorer.caves.length; row++) {
-			//PLEASE PAY ATTENTION TO THE DIFFERENCE:
-			for(int col = 0; col < CaveExplorer.caves[row].length; col++) {
-				//create a "default" cave
-				CaveExplorer.caves[row][col] = 
-						new NPCRoom("This cave has coords ("+row+","+col+")");
+			for(int row = 0; row < CaveExplorer.caves.length; row++) {
+				//PLEASE PAY ATTENTION TO THE DIFFERENCE:
+				for(int col = 0; col < CaveExplorer.caves[row].length; col++) {
+					//create a "default" cave
+					CaveExplorer.caves[row][col] = 
+							new NPCRoom("This cave has coords ("+row+","+col+")");
+				}
 			}
-		}
+			
 		//3. Replace default rooms with custom rooms
-		//--- WE WILL DO LATER
-		CaveExplorer.npcs = new NPC[1];
-		CaveExplorer.npcs[0] = new NPC();
-		CaveExplorer.npcs[0].setposition(1, 1);
+			//CaveExplorer.npcs = new NPC[];
+			//CaveExplorer.npcs[0] = new NPCMonkey();
+			//CaveExplorer.npcs[0].setposition(6, 4);
 
 		//4. Set your starting room:
-		CaveExplorer.currentRoom = CaveExplorer.caves[0][1];
-		CaveExplorer.currentRoom.enter();
+			CaveExplorer.currentRoom = CaveExplorer.caves[0][1];
+			CaveExplorer.currentRoom.enter();
+			
 		//5. Set up dooors
-		CaveRoom[][] c = CaveExplorer.caves;
-		c[0][1].setConnection(SOUTH, c[1][1], new Door());
-		/**
-		 * Special requests:
-		 * moving objects in caves
-		 * what happens when you lose?
-		 * can another object move toward you?
-		 */
-
+			CaveRoom[][] c = CaveExplorer.caves;
+			c[0][1].setConnection(SOUTH, c[1][1], new Door());
+			c[1][1].setConnection(EAST, c[1][2], new Door());
 	}
 
 
@@ -233,7 +229,6 @@ public class CaveRoom {
 			return null;
 		}
 	}
-
 
 
 }
