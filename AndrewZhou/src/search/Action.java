f0685e67fc7b258a97c4286da135971bcc472591
BT_1N3_06 <BT_1N3_06@BT_1N3_06.bktech.local>
2017-11-30 11:45:02 -0500

    11

---
+++ AndrewZhou/src/search/Action.java
@@ -0,0 +1,8 @@
+package search;
+
+public interface Action {
+
+	void act();
+	
+	
+}
---
+++ AndrewZhou/src/search/ArtificalForLoop.java
@@ -0,0 +1,26 @@
+package search;
+
+public class ArtificalForLoop {
+
+	public static void main(String[]args) {
+		forLoop(10, new Action() {
+			
+			int count = 1;
+			
+			@Override
+			public void act() {
+				System.out.println(count+ ". Hello workers!");
+				count++;
+			}
+		});
+	}
+	
+	public static void forLoop(int n, Action a) {
+		if(n == 11) {
+			
+		}else {
+			a.act();
+			forLoop(n-1, a);
+		}
+	}
+}
